508f7813a00d646cf0091bd3b696e1656299d2ee
DebuggerAndrzej <redpandaworkstation@gmail.com>
2023-02-26 22:18:12 +0100

    Added introduction and timeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
---
+++ src/data/timeline.ts
@@ -0,0 +1,26 @@
+export default [
+    {
+        year: '2021 - Today',
+        company: "Nokia",
+        title: 'Engineer, SW Development',
+        duration: 'One year',
+        details: 
+        'Software engineer with focus on Python.'
+    },
+    {
+        year: '2019 - 2021',
+        company: "Nokia",
+        title: 'Working Student',
+        duration: 'Two years',
+        details: 
+        'Here is where my adventure with Python began.',
+    },
+    {
+        year: '2015 - 2019',
+        company: "Wrocław University of Science and Technology",
+        title: 'Student',
+        duration: 'Three years',
+        details: 
+        'At University I have developed my passion to programming.',
+    },
+]
