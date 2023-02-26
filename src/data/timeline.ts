42bf6a59a9ab6f666239ad1cbaa75dd8dbc98c62
DebuggerAndrzej <redpandaworkstation@gmail.com>
2023-02-26 22:34:50 +0100

    Format code with prettier and setup CI

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,26 +1,23 @@
 export default [
-    {
-        year: '2021 - Today',
-        company: "Nokia",
-        title: 'Engineer, SW Development',
-        duration: 'One year',
-        details: 
-        'Software engineer with focus on Python.'
-    },
-    {
-        year: '2019 - 2021',
-        company: "Nokia",
-        title: 'Working Student',
-        duration: 'Two years',
-        details: 
-        'Here is where my adventure with Python began.',
-    },
-    {
-        year: '2015 - 2019',
-        company: "Wrocław University of Science and Technology",
-        title: 'Student',
-        duration: 'Three years',
-        details: 
-        'At University I have developed my passion to programming.',
-    },
-]
+  {
+    year: "2021 - Today",
+    company: "Nokia",
+    title: "Engineer, SW Development",
+    duration: "One year",
+    details: "Software engineer with focus on Python.",
+  },
+  {
+    year: "2019 - 2021",
+    company: "Nokia",
+    title: "Working Student",
+    duration: "Two years",
+    details: "Here is where my adventure with Python began.",
+  },
+  {
+    year: "2015 - 2019",
+    company: "Wrocław University of Science and Technology",
+    title: "Student",
+    duration: "Three years",
+    details: "At University I have developed my passion to programming.",
+  },
+];
