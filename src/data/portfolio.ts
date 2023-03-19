7293247a2c524b7a0fbb4a2f4c3514892836761c
DebuggerAndrzej <redpandaworkstation@gmail.com>
2023-03-19 18:38:09 +0100

    Add portfolio and contact section

---
+++ src/data/portfolio.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "This website",
+    img_url: "/assets/professional-portfolio.png",
+    stack: ["Astro", "Tailwind"],
+    link: "https://github.com/DebuggerAndrzej/portfolio_website",
+  },
+  {
+    title: "Advent of Code 2022",
+    img_url: "/assets/coding.png",
+    stack: ["Python"],
+    link: "https://github.com/DebuggerAndrzej/AoC-2022",
+  },
+  {
+    title: "Risma",
+    img_url: "/assets/crab.png",
+    stack: ["Rust", "Iced"],
+    link: "https://github.com/DebuggerAndrzej/Risma",
+  },
+];
