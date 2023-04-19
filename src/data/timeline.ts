a674e39731312515587b48632e6c4b94b58f7b7a
DebuggerAndrzej <redpandaworkstation@gmail.com>
2023-04-19 19:53:20 +0200

    Change Introduction, Timeline and add snowy bg

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,23 +1,35 @@
 export default [
   {
-    year: "2021 - Today",
-    company: "Nokia",
-    title: "Engineer, SW Development",
-    duration: "One year",
-    details: "Software engineer with focus on Python.",
+    year: "2015 - 2019",
+    company: "Wrocław University of Science and Technology",
+    title: "Student",
+    duration: "Three years",
+    details: "At University I have developed my passion to programming.",
+    addtionalInfo: ["Many programming Languages ", "Data structures", "Understanding of algorithmic complexity"],
   },
+
   {
     year: "2019 - 2021",
     company: "Nokia",
     title: "Working Student",
     duration: "Two years",
     details: "Here is where my adventure with Python began.",
+    addtionalInfo: [
+      "Developing python backend",
+      "Maintenance and small feature work in JS/TS with React and Bootstrap",
+      "Solid ground for understanding how use git and work in team",
+    ],
   },
   {
-    year: "2015 - 2019",
-    company: "Wrocław University of Science and Technology",
-    title: "Student",
-    duration: "Three years",
-    details: "At University I have developed my passion to programming.",
+    year: "2021 - Today",
+    company: "Nokia",
+    title: "Engineer, SW Development",
+    duration: "One year",
+    details: "Software engineer with focus on Python.",
+    addtionalInfo: [
+      "Writing automation scripts",
+      "Reviews for teams unfamiliar with Python",
+      "A bit of C++ tasks to extend my programming knowledge",
+    ],
   },
 ];
